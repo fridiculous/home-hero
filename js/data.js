ba52ad7cae5e9413dad4e19251040ec4e682c66a
Mary Casserly <marycasserly@gmail.com>
2015-11-08 10:19:24 -0800

    Add Frontend Static Templates & JS

---
+++ js/data.js
@@ -0,0 +1,140 @@
+{
+"records": [
+  {
+    "id": "phx0",
+    "closePrice": 123400,
+    "dateSold": "2015-10-08T22:47:20.102Z",
+    "jobValue": 56000,
+    "distanceMiles": 2.6,
+    "media": [
+      {
+        "id": "0d361c4fc59e6fed506db7e0171c2631",
+        "url": "http://photos1.zillowstatic.com/p_h/ISxzxlynolzxo70000000000.jpg",
+        "mimeType": "image/jpeg",
+        "index": 0
+      },
+      {
+        "id": "fd09afc321b677eda6382c04054a3468",
+        "url": "http://photos2.zillowstatic.com/p_h/IS1fg9ai7uke9a0000000000.jpg",
+        "mimeType": "image/jpeg",
+        "index": 1
+      },
+      {
+        "id": "f3e7e03c8b2412b8aeec72f0ef9f5c8e",
+        "url": "http://photos2.zillowstatic.com/p_h/ISp5x6wwbdzmca0000000000.jpg",
+        "mimeType": "image/jpeg",
+        "index": 2
+      },
+      {
+        "id": "61988b4e13048c01898c48f2e153ec97",
+        "url": "http://photos1.zillowstatic.com/p_h/IS9dge7jqo2cia0000000000.jpg",
+        "mimeType": "image/jpeg",
+        "index": 3
+      }
+    ]
+  },
+
+  {
+    "id": "phx1",
+    "closePrice": 213300,
+    "dateSold": "2015-11-01T22:47:20.102Z",
+    "jobValue": 36000,
+    "distanceMiles": 5.6,
+    "media": [
+      {
+        "id": "0d361c4fc59e6fed506db7e0171c2631",
+        "url": "http://photos1.zillowstatic.com/p_h/IS9dge7jqo2cia0000000000.jpg",
+        "mimeType": "image/jpeg",
+        "index": 0
+      },
+      {
+        "id": "fd09afc321b677eda6382c04054a3468",
+        "url": "http://photos1.zillowstatic.com/p_h/ISxzxlynolzxo70000000000.jpg",
+        "mimeType": "image/jpeg",
+        "index": 1
+      },
+      {
+        "id": "f3e7e03c8b2412b8aeec72f0ef9f5c8e",
+        "url": "http://photos2.zillowstatic.com/p_h/ISp5x6wwbdzmca0000000000.jpg",
+        "mimeType": "image/jpeg",
+        "index": 2
+      },
+      {
+        "id": "61988b4e13048c01898c48f2e153ec97",
+        "url": "http://photos2.zillowstatic.com/p_h/IS1fg9ai7uke9a0000000000.jpg",
+        "mimeType": "image/jpeg",
+        "index": 3
+      }
+    ]
+  },
+
+  {
+    "id": "phx2",
+    "closePrice": 515000,
+    "dateSold": "2015-10-12T22:47:20.102Z",
+    "jobValue": 66000,
+    "distanceMiles": 7.6,
+    "media": [
+      {
+        "id": "0d361c4fc59e6fed506db7e0171c2631",
+        "url": "http://photos2.zillowstatic.com/p_h/IS1fg9ai7uke9a0000000000.jpg",
+        "mimeType": "image/jpeg",
+        "index": 0
+      },
+      {
+        "id": "fd09afc321b677eda6382c04054a3468",
+        "url": "http://photos1.zillowstatic.com/p_h/ISxzxlynolzxo70000000000.jpg",
+        "mimeType": "image/jpeg",
+        "index": 1
+      },
+      {
+        "id": "f3e7e03c8b2412b8aeec72f0ef9f5c8e",
+        "url": "http://photos2.zillowstatic.com/p_h/ISp5x6wwbdzmca0000000000.jpg",
+        "mimeType": "image/jpeg",
+        "index": 2
+      },
+      {
+        "id": "61988b4e13048c01898c48f2e153ec97",
+        "url": "http://photos1.zillowstatic.com/p_h/IS9dge7jqo2cia0000000000.jpg",
+        "mimeType": "image/jpeg",
+        "index": 3
+      }
+    ]
+  },
+
+  {
+    "id": "phx3",
+    "closePrice": 385000,
+    "dateSold": "2015-10-18T22:47:20.102Z",
+    "jobValue": 66000,
+    "distanceMiles": 7.6,
+    "media": [
+      {
+        "id": "0d361c4fc59e6fed506db7e0171c2631",
+        "url": "http://photos2.zillowstatic.com/p_h/IS1fg9ai7uke9a0000000000.jpg",
+        "mimeType": "image/jpeg",
+        "index": 0
+      },
+      {
+        "id": "fd09afc321b677eda6382c04054a3468",
+        "url": "http://photos1.zillowstatic.com/p_h/ISxzxlynolzxo70000000000.jpg",
+        "mimeType": "image/jpeg",
+        "index": 1
+      },
+      {
+        "id": "f3e7e03c8b2412b8aeec72f0ef9f5c8e",
+        "url": "http://photos2.zillowstatic.com/p_h/ISp5x6wwbdzmca0000000000.jpg",
+        "mimeType": "image/jpeg",
+        "index": 2
+      },
+      {
+        "id": "61988b4e13048c01898c48f2e153ec97",
+        "url": "http://photos1.zillowstatic.com/p_h/IS9dge7jqo2cia0000000000.jpg",
+        "mimeType": "image/jpeg",
+        "index": 3
+      }
+    ]
+  }
+
+]
+}
