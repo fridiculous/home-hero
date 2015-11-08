ecbc8cc6a9d954ea3e72c501afb9e0fc82b63c9f
Simon Frid <simon.frid@livelovely.com>
2015-11-07 17:53:48 -0800

    Add Phoenix Data and Model Pulling

--- hero/scripts/approved_venders.py
+++ hero/scripts/approved_venders.py
@@ -1,39 +1,77 @@
 import requests
+import pandas as pd
+import json
 
 
 headers = {
     'cache-control': "no-cache",
     }
 
+def _get_response(url, querystring):
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    return response.text
+
 
 def get_approved_venders():
     url = "https://rets.io/api/v1/vendors/approved"
-
     querystring = {"access_token":"09d66e16b29cdd623dd1e7d0d0f1f062"}
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-
-    return response.text
+    return _get_response(url, querystring)
 
 
 def get_arizona_transactions():
     url = "https://rets.io/api/v1/armls/transactions"
-
     querystring = {"access_token":"09d66e16b29cdd623dd1e7d0d0f1f062"}
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-
-    return response.text
+    return _get_response(url, querystring)
 
 
-def get_arizona_listings():
+def get_arizona_listings(limit=100, offset=0):
     url = "https://rets.io/api/v1/armls/listings"
+    querystring = {
+        "access_token":"09d66e16b29cdd623dd1e7d0d0f1f062",
+        "state[in][0]":"AZ","limit":str(limit),"sortBy":"listDate",
+        "order":"desc","offset":str(offset)
+        }
+    return _get_response(url, querystring)
 
-    querystring = {"access_token":"09d66e16b29cdd623dd1e7d0d0f1f062","state[in][0]":"AZ","limit":"50"}
 
-    response = requests.request("GET", url, headers=headers, params=querystring)
+def get_listings_near(near="Mesa", limit=100, offset=0):
+    url = "https://rets.io/api/v1/armls/listings"
+    querystring = {
+        "access_token":"09d66e16b29cdd623dd1e7d0d0f1f062",
+        "state[in][0]":"AZ","limit":str(limit),"sortBy":"listDate",
+        "order":"desc","offset":str(offset),"near":near
+        }
+    return _get_response(url, querystring)
 
-    return response.text
+
+def batch_listings_near(near="Mesa"):
+    mesa_list = []
+    df = pd.DataFrame(json.loads(get_listings_near(offset=0))['bundle'])
+    mesa_list.append(df)
+
+    counter = 0
+    while len(df) > 0:
+        counter = counter + 1
+        print('Running job {}, last job had {} listings'.format(counter, len(df)))
+        df = pd.DataFrame(json.loads(get_listings_near(offset=100*counter))['bundle'])
+        mesa_list.append(df)
+
+    return mesa_list
+
+
+def batch_arizona_listings():
+    az_list = []
+    df = pd.DataFrame(json.loads(get_arizona_listings(offset=0))['bundle'])
+    az_list.append(df)
+
+    counter = 0
+    while len(df) > 0 and counter < 5:
+        counter = counter + 1
+        print('Running job {}, last job had {} listings'.format(counter, len(df)))
+        df = pd.DataFrame(json.loads(get_arizona_listings(offset=100*counter))['bundle'])
+        az_list.append(df)
+
+    return az_list
 
 
 if __name__=='__main__':
